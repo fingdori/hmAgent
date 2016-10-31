527986c3e3097376aeb4c4369b4155cce4c60fba
sanghyunbak <sanghyunbak@gmail.com>
2016-10-31 11:23:54 +0900

    로그인 이벤트 발생 시 가용한 ip 목록 조회 후 항목에 추가  - 최초 로그인 시 ip 할당이 안될 수도 있기 때문에 1초씩 최대 1000 리트라이 시도 가능하도록 설정 (17분 정도)

--- HyunDaiSecurityAgent/Utils.cs
+++ HyunDaiSecurityAgent/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net.NetworkInformation;
 using System.Reflection;
 using System.Xml;
 
@@ -9,6 +10,8 @@ namespace HyunDaiSecurityAgent
 {
     class Utils
     {
+        private const int RetryCountMax = 1000;
+        private const int SleepDuration = 1000; //1 second
         private static EventLog _localLog = LogManager.getLocalLog();
 
         // 다른 프로세스에서 사용하고 있는 지 체크 (이벤트 핸들러 trigger가 너무 빨라서 변경 완료 하기 전보다 먼저 실행 되어 다른 프로세스가 파일을 사용중이라는 오류 발생)
@@ -53,5 +56,38 @@ namespace HyunDaiSecurityAgent
             return xmlNodeUuid.InnerText.Replace("\r\n", "").Trim();
         }
 
+        public static String getActiveIps() {
+            
+            NetworkInterface[] nts = NetworkInterface.GetAllNetworkInterfaces();
+            DateTime now = DateTime.UtcNow;
+            String ipAddress = "";
+            int retryCount = 0;
+
+            // 최대 1000번의 retry를 1초 간격으로 실시
+            while (ipAddress.Equals("") || retryCount > RetryCountMax) {
+                foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                    {
+                        foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            {
+                                if (!ipAddress.Equals(""))
+                                {
+                                    ipAddress += ",";
+                                }
+                                ipAddress += ip.Address.ToString();
+                            }                   
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep(SleepDuration);
+                retryCount++;
+            }
+            _localLog.WriteEntry("retryCount : " + --retryCount, EventLogEntryType.Information);
+
+            return ipAddress;
+        }
     }
 }
